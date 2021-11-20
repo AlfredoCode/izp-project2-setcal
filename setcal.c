d3b2b50bee3221693b7bc5145495b503b6e58836
dancapka <94750374+dancapka@users.noreply.github.com>
2021-11-20 21:27:17 +0100

    reformat vnořených IFů v mainu

--- setcal.c
+++ setcal.c
@@ -14,99 +14,101 @@ typedef struct{
 
 
 int main(int argc, char **argv){
+    if(argc !=2){
+        fprintf(stderr,"invalid count of arguments\n");
+        return 1;   
+    }
 
-    if(argc == 2){
-        FILE *fileptr;
+
+    FILE *fileptr;
         
-        if((fileptr = fopen(argv[1],"r")) == NULL){
-            fprintf(stderr,">File '%s' could not be opened\n",argv[1]);
-            return 1;
+    if((fileptr = fopen(argv[1],"r")) == NULL){
+        fprintf(stderr,">File '%s' could not be opened\n",argv[1]);
+        return 1;
             
-        }
-        else{
-            char ForbiddenWords [21][15] =  {
-                                                {"empty"},{"true"},{"false"},{"card"},
-                                                {"complement"},{"union"},{"intersect"},{"minus"},
-                                                {"subseteq"},{"subset"},{"equals"},{"reflexive"},
-                                                {"symmetric"},{"antisymmetric"},{"transitive"},
-                                                {"function"},{"domain"},{"codomain"},{"injective"},
-                                                {"surjective"},{"bijective"}
-                                            };
+    }
+   char ForbiddenWords [21][15] =  {
+                                        {"empty"},{"true"},{"false"},{"card"},
+                                        {"complement"},{"union"},{"intersect"},{"minus"},
+                                        {"subseteq"},{"subset"},{"equals"},{"reflexive"},
+                                        {"symmetric"},{"antisymmetric"},{"transitive"},
+                                        {"function"},{"domain"},{"codomain"},{"injective"},
+                                        {"surjective"},{"bijective"}
+                                        };
             
 
                 //printf("%c %s\n",ForbiddenWords[2][3], ForbiddenWords[4]);
-            
-            char c;
-            int CharCount = 0;
-            int U_MAX = 0;
-            bool isUniverzum = false;
-            char buffer[100] = {0};
-            int i = 0;
-            int z = 0;
-            int WordCount = 0;
-             
-            set_t U;
-            
-            while((c = fgetc(fileptr)) != EOF){
-                printf("%c\n",c);
-                if(c == ' '){
-                    buffer[CharCount] = '\0';
-                    if(WordCount == 0){
-                        printf("Aho");
-                        U.set = malloc(sizeof(char) * MAX_LETTERS);
+                
+    char c;
+    int CharCount = 0;
+    int U_MAX = 0;
+    bool isUniverzum = false;
+    char buffer[100] = {0};
+    int i = 0;
+    int z = 0;
+    int WordCount = 0;
+               
+    set_t U;
+                
+    while((c = fgetc(fileptr)) != EOF){
+        printf("%c\n",c);
+        if(c == ' '){
+            buffer[CharCount] = '\0';
+            if(WordCount == 0){
+                printf("Aho");
+                U.set = malloc(sizeof(char) * MAX_LETTERS);
 
-                    }
-                    else{
-                        U.set = realloc(U.set,sizeof(char) * MAX_LETTERS * (WordCount+1));
+            }
+            else{
+                U.set = realloc(U.set,sizeof(char) * MAX_LETTERS * (WordCount+1));
                         
-                    }
-                    if(U.set == NULL){
+            }
+            if(U.set == NULL){
 
-                        fprintf(stderr, "Not enough memory1");
+                fprintf(stderr, "Not enough memory1");
+                return 1;
+            }
+
+
+            U.set[WordCount] = buffer;
+            U.set[WordCount] = malloc(sizeof(char) * strlen(buffer));
+            if(U.set[WordCount] == NULL){
+
+                fprintf(stderr, "Not enough memory");
+            }     
+                    
+            WordCount++;
+            U.WordCount = WordCount;
+                    
+        }
+        if(c == '\n'){
+            CharCount = 0;
+            isUniverzum = false;
+                    
+            WordCount = 0;
+            continue;                      
+        }
+                //if (CharCount == 0){
+            if(c == 'U'){   
+                if(CharCount == 0){
+                    isUniverzum = true;
+                    CharCount++;
+                    if(fgetc(fileptr) != ' '){
+                                
+                        fprintf(stderr,"Space after initializator error\n");
                         return 1;
                     }
-
-
-                    U.set[WordCount] = buffer;
-                    U.set[WordCount] = malloc(sizeof(char) * strlen(buffer));
-                    if(U.set[WordCount] == NULL){
-
-                        fprintf(stderr, "Not enough memory");
-                    }     
-                    
-                    WordCount++;
-                    U.WordCount = WordCount;
-                    
-                }
-                if(c == '\n'){
-                    CharCount = 0;
-                    isUniverzum = false;
-                    
-                    WordCount = 0;
-                    continue;                      
-                }
-                //if (CharCount == 0){
-                    if(c == 'U'){   
-                        if(CharCount == 0){
-                            isUniverzum = true;
-                            CharCount++;
-                            if(fgetc(fileptr) != ' '){
-                                
-                                fprintf(stderr,"Space after initializator error\n");
-                                return 1;
-                            }
                             //printf("I found U definition\n");
-                            continue;
-                        } 
-                        else{
+                    continue;
+                } 
+                else{
                             
-                            fprintf(stderr,"Error on token U %d\n",CharCount);
-                            return 1;
-                        }
-                        
-                    }
-                    buffer[CharCount] = c;
-                    CharCount++;
+                    fprintf(stderr,"Error on token U %d\n",CharCount);
+                    return 1;
+                }
+            }
+            buffer[CharCount] = c;
+            CharCount++;
 
 
 
@@ -156,31 +158,19 @@ int main(int argc, char **argv){
                     } */  
                 //CharCount++;
                 
-            } 
-            for(i = 0; i < U.WordCount;i++){
-                        free(U.set[i]);
+    } 
+    for(i = 0; i < U.WordCount;i++){
+        free(U.set[i]);
 
-            }
+    }
             
-            free(U.set);
+    free(U.set);
             
 
-            printf("Debug\n");
-        }
+    printf("Debug\n");
         
 
-        fclose(fileptr);
-    }
-
-
-
-
-    else if(argc == 1){
-        fprintf(stderr,"Please provide input FILE\n");
-    }
-    else{
-        fprintf(stderr,"Too many arguments\n");
-    }
+    fclose(fileptr);
 
     return 0;
 }
